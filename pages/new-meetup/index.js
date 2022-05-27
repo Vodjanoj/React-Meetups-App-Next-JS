19036619f5149b96a1eb67345b39de085b67a4ad
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-27 11:36:37 +0300

    Adding the New Meetup Form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,2 +1,13 @@
 // our domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
+const NewMeetupPage = () => {
+
+  const onAddMeetupHandler = (enteredMeetupDate) => {
+      console.log(enteredMeetupDate)
+  };
+
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
+};
+
+export default NewMeetupPage;
