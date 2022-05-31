3747cf240d1f82f0e0ff3571cce43483a3a1497e
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-31 11:51:59 +0300

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  // when we accept it on getStaticProps, context will not hold request and response, but it will, for example, have a params key.
+  // and that will be an object where our identifiers between the square brackets ([meetupId]) will be properties and the values will be the actual values
+  // encoded in the URL. And that would then be the concrete meetup ID for which we're displaying this meetup.
+  const meetupId = context.params.meetupId;
+  console.log(meetupId)
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
