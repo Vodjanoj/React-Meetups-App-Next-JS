1c0847025a21ce91aaf047aac71b60b186270977
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-31 15:47:47 +0300

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 // POST /api/new-meetup
 
@@ -5,13 +6,27 @@
 // The request object contains data about the incoming request.
 // The response object will be needed for sending back a response.
 
-function handler(req, res) {
+async function handler(req, res) {
   // Now, from that request object, we can get things like the headers or the request body and also the request method, route a method property here.
   // This allows us to find out which kind of request was sent.
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://vladimirs:vovan2001@cluster0.1nvkxcj.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups-collection");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
